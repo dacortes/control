c6c1d818a841d116dedccf88759fe237ffa26a0d
lbDYX <dannyel.cortesr@gmail.com>
2023-03-08 22:04:35 +0100

    add funtions date and hours

---
+++ src/test_hour_date/init.ino
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dacortes <dacortes@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/05 15:23:45 by dacortes          #+#    #+#             */
+/*   Updated: 2023/03/05 15:23:45 by dacortes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"hidroteg.h"
+
+void    init_pins()
+{
+    pinMode(relay_one, OUTPUT);
+}
+
+void    init_status()
+{
+    digitalWrite(relay_one, HIGH);
+}
+
+void    init_events()
+{
+    btn_auto.attachPop(event_relay_one, relay_one);
+}
+
+void    init_date(int day, int month, int year)
+{
+    day = now.date();
+    month = now.month();
+    year = now.year();
+    /* envio de datos */
+    n_day.setValue(day);
+    n_month.setValue(month);
+    n_year.setValue(year);
+}
+
+void    init_hour(int hour, int minute, int second)
+{
+    hour = now.hour();
+    minute = now.minute();
+    second = now.second();
+    /* envio de datos */
+    n_hour.setValue(hour);
+    n_minu.setValue(minute);
+    n_seco.setValue(second);
+}
---
+++ src/test_hour_date/main.ino
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.ino                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dacortes <dacortes@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/28 20:38:39 by dacortes          #+#    #+#             */
+/*   Updated: 2023/02/28 20:38:39 by dacortes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"hidroteg.h"
+
+/* init pines*/
+void setup ()
+{
+    nexInit();
+    init_pins();
+    init_status();
+    init_events();
+    rtc.setDateTime(dt);
+}
+
+void loop()
+{
+  init_date(day, month, year);
+  init_hour(hour, minute, second);
+  nexLoop(nex_listen_list);
+}
+
